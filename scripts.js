7f555cb324fc7245ee9861618fe16ef9a91e1b0a
LuizRicardo29 <luiz.rgn2@gmail.com>
2023-09-29 13:53:19 -0300

    adicionado caixa do resultado abaixo do botão

--- scripts.js
+++ scripts.js
@@ -5,8 +5,11 @@ const min = Math.ceil(document.querySelector(".input-min").value)
 const max = Math.floor(document.querySelector(".input-max").value)
 
 const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    
-alert(result)
+
+const resultBox = document.querySelector(".result-box")
+
+resultBox.innerHTML = result
 }
 
 generateButton.addEventListener("click", generateNumber)
+

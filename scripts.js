7adf1fcab7c1944082629015fb54fa801ad7d66d
LuizRicardo29 <luiz.rgn2@gmail.com>
2023-09-29 12:51:49 -0300

    Sorteador Devclub

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const generateButton = document.querySelector(".button-generate")
+
+function generateNumber() {
+const min = Math.ceil(document.querySelector(".input-min").value)
+const max = Math.floor(document.querySelector(".input-max").value)
+
+const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+alert(result)
+}
+
+generateButton.addEventListener("click", generateNumber)

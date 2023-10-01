19ad27eddca49ed038327a77ed93b871f325faac
LuizRicardo29 <luiz.rgn2@gmail.com>
2023-10-01 19:24:41 -0300

    Adicionado operador de comparação + alert

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 const generateButton = document.querySelector(".button-generate")
 
 function generateNumber() {
-const min = Math.ceil(document.querySelector(".input-min").value)
-const max = Math.floor(document.querySelector(".input-max").value)
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("ATENÇÃO! O valor mínimo tem que ser menor que o valor máximo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        const resultBox = document.querySelector(".result-box")
 
-const resultBox = document.querySelector(".result-box")
-
-resultBox.innerHTML = result
+        resultBox.innerHTML = result
+    }
 }
 
 generateButton.addEventListener("click", generateNumber)
